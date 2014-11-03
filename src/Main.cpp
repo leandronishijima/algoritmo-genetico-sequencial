b6dc758e141e9d300b5b261e9a2d88b85b8b7428
Leandro Nishijima <leandro.nishijima@gmail.com>
2014-11-03 01:38:47 -0200

    Removido métodos de teste sem arquivo de entrada

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,73 +11,6 @@
 
 using namespace std;
 
-Grafo populacaoInicial[2];
-vector<int> numeroVertices;
-
-int arraySize() {
-	return numeroVertices.size();
-}
-
-void adicionaVerticesComMesmaCor(Grafo grafo, int corDosVertices) {
-	for (int i=0; i < arraySize(); ++i)
-		grafo.adicionaVertice(numeroVertices[i], corDosVertices);
-}
-
-void adicionaVerticesComCoresRandomicas(Grafo grafo) {
-	for (int i=0; i < arraySize(); ++i)
-		grafo.adicionaVertice(numeroVertices[i], rand() % 4);
-}
-
-void adicionaArestasTeste(Grafo grafo) {
-	grafo.adicionaArestaComCor1(0, 1);
-	grafo.adicionaArestaComCor1(0, 2);
-	grafo.adicionaArestaComCor1(0, 3);
-	grafo.adicionaArestaComCor1(1, 0);
-	grafo.adicionaArestaComCor1(1, 2);
-	grafo.adicionaArestaComCor1(1, 4);
-	grafo.adicionaArestaComCor1(2, 0);
-	grafo.adicionaArestaComCor1(2, 1);
-	grafo.adicionaArestaComCor1(2, 5);
-	grafo.adicionaArestaComCor1(3, 0);
-	grafo.adicionaArestaComCor1(3, 5);
-	grafo.adicionaArestaComCor1(4, 1);
-	grafo.adicionaArestaComCor1(4, 5);
-	grafo.adicionaArestaComCor1(5, 3);
-	grafo.adicionaArestaComCor1(5, 4);
-	grafo.adicionaArestaComCor1(5, 2);
-}
-
-Grafo geraPrimeiroGrafoTeste() {
-	Grafo grafo = Grafo();
-	adicionaArestasTeste(grafo);
-	return grafo;
-}
-
-Grafo geraSegundoGrafoTeste() {
-	Grafo grafo = Grafo();
-	adicionaVerticesComCoresRandomicas(grafo);
-	adicionaArestasTeste(grafo);
-	return grafo;
-}
-
-void criaPopulacaoInicial() {
-	populacaoInicial[0] = geraPrimeiroGrafoTeste();
-	populacaoInicial[1] = geraSegundoGrafoTeste();
-}
-
-void testeSemArquivo() {
-	numeroVertices.push_back(0);
-	numeroVertices.push_back(1);
-	numeroVertices.push_back(2);
-	numeroVertices.push_back(3);
-	numeroVertices.push_back(4);
-	numeroVertices.push_back(5);
-
-	criaPopulacaoInicial();
-	AlgoritmoGenetico algoritmo = AlgoritmoGenetico(populacaoInicial);
-	algoritmo.executaAlgoritmo();
-}
-
 void testeComArquivo() {
 	GeradorDeGrafo gerador = GeradorDeGrafo("grafo5.txt");
 	gerador.iteraArquivoGerandoGrafoComCorUnica();
@@ -85,7 +18,6 @@ void testeComArquivo() {
 }
 
 int main() {
-//	testeSemArquivo();
 	testeComArquivo();
 	return 0;
 }
